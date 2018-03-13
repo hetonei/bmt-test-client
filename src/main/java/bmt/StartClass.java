b95ad1c383834ce28285faca9645c74c1d99c592
hetonei <hetonei@gmail.com>
2018-03-13 14:09:26 +0200

    Init

---
+++ src/main/java/bmt/StartClass.java
@@ -0,0 +1,27 @@
+package bmt;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLConnection;
+import java.nio.charset.StandardCharsets;
+
+public class StartClass {
+    public static void main(String[] args) throws IOException{
+        URL url = new URL("http://localhost:8080/users");
+        URLConnection con = url.openConnection();
+        HttpURLConnection http = (HttpURLConnection)con;
+        http.setRequestMethod("POST"); // PUT is another valid option
+        http.setDoOutput(true);
+        byte[] out = "{\"Username\":\"KlimenkoPidar\",\"Rank\":\"357\"}" .getBytes(StandardCharsets.UTF_8);
+        int length = out.length;
+
+        http.setFixedLengthStreamingMode(length);
+        http.setRequestProperty("Content-Type", "application/json; charset=UTF-8");
+        http.connect();
+        try(OutputStream os = http.getOutputStream()) {
+            os.write(out);
+        }
+    }
+}
